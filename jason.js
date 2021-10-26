8697a42e438d485188526bea6648409758bfea3a
Rurucode <rubenlage12@gmail.com>
2021-10-26 14:03:27 +0200

    Actualizo

---
+++ jason.js
@@ -0,0 +1,49 @@
+
+    // Partiendo del siguiente JSON
+
+       let elJson = {
+     "localidade 1": {
+        "Continente": "África",
+        "País": "Angola",
+        "Capital": "Luanda"
+        },
+     "localidade 2": {
+        "Continente": "América do Norte",
+        "País": "Estados Unidos",
+        "Capital": "Washington DC"
+        },
+     "localidade 3": {
+        "Continente": "América Central",
+        "País": "México",
+        "Capital": "Cidade do México"
+        },
+     "localidade 4": {
+        "Continente": "América do Sul",
+        "País": "Brasil",
+        "Capital": "Brasília"
+        },
+     "localidade 5": {
+        "Continente": "Europa",
+        "País": "Espanha",
+        "Capital": "Madri"
+        },
+     "localidade 6": {
+        "Continente": "Europa",
+        "País": "Alemanha",
+        "Capital": "Berlim"
+        },
+     "localidade 7": {
+        "Continente": "Oceania",
+        "País": "Austrália",
+        "Capital": "Camberra"
+        },
+    "localidade 8": {
+        "Continente": "Ásia",
+        "País": "Japão",
+        "Capital": "Tóquio"
+        }
+    }
+    
+
+    // Código para obtener el país de la localidade 8
+    console.log(elJson."localidade 8"."País")
